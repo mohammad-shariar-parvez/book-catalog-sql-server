a5b5323707c2c6b5bc3946198cf0aeef7b73c3d1
Piaowei <msptonmoy@gmail.com>
2023-09-08 06:00:51 +0600

    fix(error): add pat

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { AuthRoutes } from '../modules/auth/auth.route';
+import { UserRoutes } from '../modules/user/user.route';
 
 
 const router = express.Router();
@@ -9,6 +10,10 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
   }
 ];
 
