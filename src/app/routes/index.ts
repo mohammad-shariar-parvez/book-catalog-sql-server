3994bb917d8fca893feb1176154fb9a1c8a7e085
Piaowei <msptonmoy@gmail.com>
2023-09-10 08:17:35 +0600

    feat(routes): complete routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { AuthRoutes } from '../modules/auth/auth.route';
 import { BookRoutes } from '../modules/book/book.route';
 import { CategoryRoutes } from '../modules/catogory/category.route';
+import { OrderRoutes } from '../modules/order/order.route';
+import { ProfileRoutes } from '../modules/profile/profile.route';
 import { UserRoutes } from '../modules/user/user.route';
 
 
@@ -24,6 +26,14 @@ const moduleRoutes = [
   {
     path: '/books',
     route: BookRoutes,
+  },
+  {
+    path: '/orders',
+    route: OrderRoutes,
+  },
+  {
+    path: '/profile',
+    route: ProfileRoutes,
   }
 ];
 
