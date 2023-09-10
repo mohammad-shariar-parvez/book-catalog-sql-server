08a1f741294934639fa5178d07a86cc40ea24c3a
Piaowei <msptonmoy@gmail.com>
2023-09-10 08:15:10 +0600

    feat(order): complete order module

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import httpStatus from "http-status";
