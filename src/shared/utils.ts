35aaa3416b9542d878391f9feaab14cb20841489
Piaowei <msptonmoy@gmail.com>
2023-09-10 08:21:33 +0600

    feat(utils): utils function added

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const asyncForEach = async (array: any[], callback: any) => {
 	if (!Array.isArray(array)) {
 		throw new Error("Expected an array");
